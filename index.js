45d855967e6b8ec07f7d8f1a7c272b7612595cb0
LisaWijffels <r0657589@student.thomasmore.be>
2018-03-16 12:01:38 +0100

    Created index.js

--- index.js
+++ index.js
@@ -8,3 +8,19 @@
 
 
 /* luister naar poort */
+
+//const chalk = require('chalk');
+//console.log(chalk.blue("JS")); // werkt niet?
+
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.use(bodyParser.json() );
+app.use('/', require('./routes/home') );
+app.use('/kweeni', require('./routes/kweeni') );
+app.use('/watis', require('./routes/watis') );
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
