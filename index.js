a4f1e0ffdfbc4b51a7d034d7b1f959031f48bd94
LisaWijffels <r0657589@student.thomasmore.be>
2018-03-16 13:04:47 +0100

    Created routes/home.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.set('view engine', 'pug');
 
 app.use(bodyParser.json() );
 app.use('/', require('./routes/home') );
-app.use('/kweeni', require('./routes/kweeni') );
-app.use('/watis', require('./routes/watis') );
+//app.use('/kweeni', require('./routes/kweeni') );
+//app.use('/watis', require('./routes/watis') );
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
--- routes/home.js
+++ routes/home.js
@@ -1,1 +1,9 @@
-/* routes */
+
+const express = require('express')
+const router = express.Router();
+
+router.get('/', function(req, res){
+    res.send('Hello Home!');
+});
+
+module.exports = router;
