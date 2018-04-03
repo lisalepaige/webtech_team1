4f6d1822e09c04c5461ec79e5fc742d7c566081b
Caroline VG <caroline.vangossum@live.be>
2018-04-03 15:53:21 +0200

    test connection with mongodb - get item from collection

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 const express = require('express')
 const router = express.Router();
 
+/* mongodb */
+var mongo = require('mongodb');
+var assert = require('assert'); // for errors and validation
+
+var url = 'mongodb://localhost:27017/webtech';
+
 /* home */
 router.get('/', function(req, res){
-    res.render('./home', {title: 'Home'}); 
+ // test connection to database
+ mongo.connect(url, function (err, db) {
+    assert.equal(null, err);
+    var cursor = db.db('webtech').collection('test').find();
+    cursor.forEach(function (doc, err) {
+      assert.equal(null, err);
+      console.log(doc); 
+    }, function () {
+      db.close();
+      res.render('./home', {title: 'Home'}); 
+    });
+  });
 });
 
 /* kweeni */
