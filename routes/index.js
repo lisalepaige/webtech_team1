f2f08286e2f1aa246852c3b5d34678cf0956418c
Caroline VG <caroline.vangossum@live.be>
2018-04-03 19:26:25 +0200

    test connection with mongodb - insert item into collection

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var assert = require('assert'); // for errors and validation
 
 var url = 'mongodb://localhost:27017/webtech';
 
-/* home */
+/* GET home */
 router.get('/', function(req, res){
  // test connection to database
  mongo.connect(url, function (err, db) {
@@ -23,14 +23,41 @@ router.get('/', function(req, res){
   });
 });
 
-/* kweeni */
+/* GET kweeni */
 router.get('/kweeni', function(req, res){
     res.render('./kweeni', {title: 'kweeni'}); 
+
 });
 
-/* wat is */
+/* GET wat is */
 router.get('/watis', function(req, res){
     res.render('./watis', {title: 'watis'}); 
 });
+
+/* POST kweeni */
+router.post('/kweeni', function (req, res, next) {
+    // create item
+    console.log(req.body.vraag__input);
+    var item = {
+      question: req.body.vraag__input, 
+      author: req.body
+    };
+
+    console.log(item); 
+    // connect to mongo db
+    mongo.connect(url, function (err, db) {
+      assert.equal(null, err);
+      // acces database, use collection to insert item 
+      db.db('webtech').collection('test').insertOne(item, function (err, result) {
+        // callback (if no errors)
+        assert.equal(null, err);
+        console.log('Item inserted');
+        console.log(item);
+        db.close();
+      });
+    });
+    // redirect to home page
+    res.redirect('/');
+  });  
 
 module.exports = router;
