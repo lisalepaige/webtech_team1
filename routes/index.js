1e37e36f796cc734834c2a50915c988a68b2038a
Caroline VG <caroline.vangossum@live.be>
2018-04-11 14:31:34 +0200

    insert question into database and export to kweeni.pug live

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router();
 
-var texttoshow = ""; 
+var texttoshow = "";
 var passport = "";
 
 /* mongodb */
@@ -19,79 +19,61 @@ router.get('/', function (req, res) {
 
 /* GET kweeni + data */
 router.get('/kweeni', function (req, res) {
-      mongo.connect(url, function (err, db) {
-        assert.equal(null, err);
-        db.db('webtech').collection('question').find().toArray(function(err, result){
-          console.log(result); 
-          if (err){
-            res.send(err); 
-          } else if (result.lenght=!0){ // result is not empty
-            res.render('kweeni', {"questionslist": result});
-            console.log(result); 
-          } else {
-            res.send('no questions found'); 
-          }
-          });
+  mongo.connect(url, function (err, db) {
+    assert.equal(null, err);
+    db.db('webtech').collection('question').find().toArray(function (err, result) {
+      console.log("_____________________________");
+      if (err) {
+        res.send(err);
+      } else if (result.lenght = !0) { // result is not empty
+        res.render('kweeni', {
+          "questionslist": result
         });
-      });
+        console.log(result);
+      } else {
+        res.send('no questions found');
+      }
+    });
+  });
+});
 
-      /* GET wat is */
-      router.get('/watis', function (req, res) {
-        res.render('./watis', {
-          title: 'watis'
-        });
-      });
+/* GET wat is */
+router.get('/watis', function (req, res) {
+  res.render('./watis', {
+    title: 'watis'
+  });
+});
 
-      /* POST wat is */
-      router.post('/watis', function (req, res) {
+/* POST wat is */
+router.post('/watis', function (req, res) {
 
-      });
-
-
-      /* POST kweeni + save data  */
-      router.post('/kweeni', function (req, res, next) {
-        // create item
-        console.log(req.body.question__input);
-        var item = {
-          question: req.body.question__input,
-          author: "tester"
-        };
-
-        console.log(item);
-        // connect to mongo db
-        mongo.connect(url, function (err, db) {
-          assert.equal(null, err);
-          // acces database, use collection to insert item 
-          db.db('webtech').collection('test').insertOne(item, function (err, result) {
-            // callback (if no errors)
-            assert.equal(null, err);
-            console.log('Item inserted');
-            console.log(item);
-            db.close();
-          });
-        });
-        // redirect to home page
-        res.redirect('/');
-      });
-
-      
-
-      /*router.get('/kweeni', function (req, res) {
-        mongo.connect(url, function (err, db) {
-          assert.equal(null, err);
-          db.db('webtech').collection('question').find().toArray(function(err, result){
-            console.log(result); 
-            if (err){
-              res.send(err); 
-            } else if (result.lenght=!0){ // result is not empty
-              res.render('kweeni', {"questionslist": result});
-            } else {
-              res.send('no questions found'); 
-            }
-            db.close();
-          });
-        });
-      });*/
+});
 
 
-      module.exports = router;
+/* POST kweeni + save data  */
+router.post('/kweeni', function (req, res, next) {
+  // create item
+  //console.log(req.body.question__input);
+  var item = {
+    text: req.body.question__input,
+    user_id: 1
+  };
+
+  console.log(item);
+  // connect to mongo db
+  mongo.connect(url, function (err, db) {
+    assert.equal(null, err);
+    // acces database, use collection to insert item 
+    db.db('webtech').collection('question').insertOne(item, function (err, result) {
+      // callback (if no errors)
+      assert.equal(null, err);
+      console.log('Item inserted');
+      //console.log(item);
+      db.close();
+    });
+  });
+  // redirect to home page
+  res.redirect('/kweeni');
+});
+
+module.exports = router;
