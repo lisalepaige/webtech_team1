38288a0216e4ed235d3e07847ec04ab547419ad3
Caroline VG <caroline.vangossum@live.be>
2018-04-13 16:30:24 +0200

    create new page by kweeni/:id

--- routes/index.js
+++ routes/index.js
@@ -1,43 +1,81 @@
 const express = require('express')
 const router = express.Router();
 var passport = "";
-var mongoose = require('mongoose'); 
+var mongoose = require('mongoose');
 
 // connection
-mongoose.connect('mongodb://localhost:27017/webtech', {autoIndex: false});
-var Schema = mongoose.Schema; 
+mongoose.connect('mongodb://localhost:27017/webtech', {
+  autoIndex: false
+});
+var Schema = mongoose.Schema;
 
 // blueprint (define layout)
 var questionsDataSchema = new Schema({
-  text: {type: String, required: true},
-  likes: {type: Number},
+  text: {
+    type: String,
+    required: true
+  },
+  likes: {
+    type: Number
+  },
+  search_term: {
+    type: String
+  },
   user: {
-    _id: {type: Number},
-    name: {type: String},
-    img: {type: String}
+    _id: {
+      type: Number
+    },
+    name: {
+      type: String
+    },
+    img: {
+      type: String
+    }
   },
   answers: [{
-    _id: {type: Number},
-    text: {type: String},
+    _id: {
+      type: Number
+    },
+    text: {
+      type: String
+    },
     user: {
-      _id: {type: Number},
-      name: {type: String},
-      img: {type: String}
+      _id: {
+        type: Number
+      },
+      name: {
+        type: String
+      },
+      img: {
+        type: String
+      }
     },
     comments: [{
-      _id: {type: Number},
-      text: {type: String},
+      _id: {
+        type: Number
+      },
+      text: {
+        type: String
+      },
       user: {
-        _id: {type: Number},
-        name: {type: String},
-        img: {type: String}
+        _id: {
+          type: Number
+        },
+        name: {
+          type: String
+        },
+        img: {
+          type: String
+        }
       }
     }]
   }]
-}, {collection: 'testje'}); // stores data in collection
+}, {
+  collection: 'testje'
+}); // stores data in collection
 
 // create model of that blueprint
-var QuestionsData = mongoose.model('QuestionsData', questionsDataSchema) 
+var QuestionsData = mongoose.model('QuestionsData', questionsDataSchema)
 
 /* GET home */
 router.get('/', function (req, res) {
@@ -48,53 +86,49 @@ router.get('/', function (req, res) {
 
 /* GET kweeni + data */
 router.get('/kweeni', function (req, res) {
-  
-/* count for id NOT USED */
-QuestionsData.count({}, function (error, result) {
-  if (error){
-    console.log(error);
-  } else {
-    count = result; 
-  }
-});
 
-  QuestionsData.find().sort({datefield: -1})
-    .then (function(result){
-      console.log(result); 
-      res.render('kweeni', {questionslist: result}); 
+  /* count for id NOT USED */
+  QuestionsData.count({}, function (error, result) {
+    if (error) {
+      console.log(error);
+    } else {
+      count = result;
+    }
+  });
+
+  QuestionsData.find().sort({
+      datefield: -1
+    })
+    .then(function (result) {
+      console.log(result);
+      res.render('kweeni', {
+        questionslist: result
+      });
     });
 });
 
-/* GET wat is */
-router.get('/watis', function (req, res) {
-  QuestionsData.findOne({_id: "5acf970d5c50916489a2937c"})
-    .then (function(result){
-      console.log(result); 
-      res.render('watis', {question: result}); 
-    });
-});
-
-/* GET specifieke vraag */
-router.get('/watis/1', function (req, res) {
-  /*QuestionsData.findOne({_id: "5acf970d5c50916489a2937c"})
-    .then (function(result){
-      console.log(result); 
-      res.render('watis', {question: result}); 
-    });*/
-    /* FETCH BY ID */
-    module.exports.fetchOne = (req, res) => {
-      console.log(req.params.id); 
-      res.render('watis', {}); 
-      /*if (student) {
-          res.status(200).json(student);
+/* GET wat is + id */
+router.get('/kweeni/:id', function (req, res) {
+  var id = req.params.id;
+  QuestionsData.findOne({
+      search_name: id
+    })
+    .then(function (result) {
+      if (result == null) {
+        res.render('error', {
+          message: 'id not found'
+        });
       } else {
-          res.status(404).json({
-              error: 404,
-              message: 'Not Found'
-          });
-      }*/
-    };
+        console.log(result);
+        res.render('watis', {
+          title: id,
+          question: result
+        });
+      }
+    });
 });
+
+
 
 /* POST wat is */
 router.post('/watis', function (req, res) {
@@ -107,6 +141,7 @@ router.post('/kweeni', function (req, res, next) {
   var item = {
     text: req.body.question__input,
     likes: 0,
+    search_term: req.body.question__input.split(" ").join("-"),
     user: {
       _id: 1,
       name: "Caroline",
@@ -115,8 +150,8 @@ router.post('/kweeni', function (req, res, next) {
   };
 
   // create instance of model 
-  var data = new QuestionsData(item); 
-  data.save(); 
+  var data = new QuestionsData(item);
+  data.save();
   res.redirect('/kweeni');
 });
 
