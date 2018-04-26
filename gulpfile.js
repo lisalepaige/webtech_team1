058a1faefddcd2ed1848eb804918e79066f332f1
Lisa Lepaige <lisa_lepaige@hotmail.com>
2018-04-26 12:39:09 +0200

    gulp sass, sasswatch, nodemon, minifycss

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const nodemon = require('gulp-nodemon');
+const minifyCSS = require('gulp-minify-css');
+
+gulp.task('default', ['sass:watch', 'sass', 'startNodemon']);
+ 
+gulp.task('sass', function () {
+  return gulp.src('./public/scss/app.scss')
+    .pipe(sass({outputStyle: 'compress'}).on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./public/scss/**/*.scss', ['sass']);
+});
+
+gulp.task('startNodemon', function () {
+    nodemon({
+      script: 'app.js'
+    , ext: 'js pug'
+    , env: { 'NODE_ENV': 'development' }
+    })
+});
+
+gulp.task('minifyCss', function() {
+    return gulp.src('./public/*.css')
+    .pipe(minifyCSS({keepBreaks:false}))
+    .pipe(gulp.dest('./public/css/'))
+});
