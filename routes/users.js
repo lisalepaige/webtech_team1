41a1fb390954e70020ee7f9bbe45b0eff2c98214
Caroline VG <caroline.vangossum@live.be>
2018-05-01 15:25:56 +0200

    test facebook

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose'); 
 
 /* GET users listing. */
 router.get('/', ensureAuthenticated, function(req, res, next) {
