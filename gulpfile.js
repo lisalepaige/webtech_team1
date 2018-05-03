ae0c2a94b6364979f9e7c69d6cf51d9c19c0ce4f
Lisa Lepaige <lisa_lepaige@hotmail.com>
2018-05-03 14:04:44 +0200

    gulpfile aangepast

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const nodemon = require('gulp-nodemon');
-const minifyCSS = require('gulp-minify-css');
 
 gulp.task('default', ['sass:watch', 'sass', 'startNodemon']);
  
