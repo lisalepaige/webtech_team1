5f3e03a0bbd66449e48ec543c9ac8803c2d37a6c
Lisa Lepaige <lisa_lepaige@hotmail.com>
2018-05-06 12:04:42 +0200

    scope

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -15,7 +15,7 @@ router.get('/logout', (req, res) => {
 
 //auth with facebook
 router.get('/facebook',passport.authenticate('facebook', {
-    scope: 'id,email,first_name,last_name,picture'
+    scope: 'email,first_name,last_name,picture'
 }));
 
 module.exports = router;
