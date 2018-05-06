42d43d595136df8041905156892f115ced6202f4
Lisa Lepaige <lisa_lepaige@hotmail.com>
2018-05-06 14:08:55 +0200

    testing fb online

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -20,8 +20,7 @@ router.get('/facebook',passport.authenticate('facebook', {
 
 //callback route for fb to redirect to
 //comes back with code where the info is in
-router.get('/facebook/redirect', 
-    passport.authenticate('facebook'),
+router.get('/facebook/redirect',
     (req, res) => {
         res.send('you reached the callback uri');
 });
