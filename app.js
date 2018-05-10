830ac30e2bbd4e9aee16988119ccc90205d05531
LisaWijffels <r0657589@student.thomasmore.be>
2018-05-10 09:32:13 +0200

    Inserted config files, db config

--- app.js
+++ app.js
@@ -12,12 +12,19 @@ var app = express();
 
 var cookieSession = require('cookie-session'); 
 
+var config = require('config');
+var dbHost = config.get('Connection.dbConfig.host');
+
 var MongoClient = require('mongodb').MongoClient;
 var local = "mongodb://localhost:27017";
 
 //connection
 var uri = "mongodb://Admin:4dm!n@gettingstarted-shard-00-00-jbvu6.mongodb.net:27017,gettingstarted-shard-00-01-jbvu6.mongodb.net:27017,gettingstarted-shard-00-02-jbvu6.mongodb.net:27017/dbkweeni?ssl=true&replicaSet=GettingStarted-shard-0&authSource=admin";
-mongoose.connect(uri);
+mongoose.connect(dbHost);
+if (config.has('optionalFeature.detail')) {
+  var detail = config.get('optionalFeature.detail');
+  
+}
 
 // require session and set secret
 /*var session = (require('express-session')({
