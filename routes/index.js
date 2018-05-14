f4814de1b948c73d8d4c52fdee38b40d8bb8b8e9
Caroline VG <caroline.vangossum@live.be>
2018-05-14 16:54:42 +0200

    test timezone

--- routes/index.js
+++ routes/index.js
@@ -109,7 +109,7 @@ router.get('/kweeni/:id', function (req, res) {
           message: 'id not found'
         });
       } else {
-        var minutes = result.current_date.getHours()+2;
+        var minutes = result.current_date.getMinutes()-120;
         var countLikes = result.likes.length;
         var finalCount = countLikes -1;
         res.render('watis', {
