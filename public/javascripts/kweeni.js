64d46bf70cf8776e5e80a380bb061af92c890c02
LisaWijffels <r0657589@student.thomasmore.be>
2018-05-16 14:31:28 +0200

    Fixed CSS new question

--- public/javascripts/kweeni.js
+++ public/javascripts/kweeni.js
@@ -36,13 +36,13 @@ document.querySelector(".question__btn--btn").addEventListener("click", function
 
 function addQuestion(user, img, search_name, text){
     var container = document.createElement("div");
-    container.classList.add(".topics__question");
+    container.classList.add("topics__question");
 
     var blockLikes = document.createElement("div");
-    blockLikes.classList.add(".topics__question--likes");
+    blockLikes.classList.add("topics__question--likes");
 
     var likeImage = document.createElement("img");
-    likeImage.classList.add(".topics__question--img");
+    likeImage.classList.add("topics__question--img");
     likeImage.src = 'img/icon-cool.svg';
 
     var likes = document.createElement("span");
@@ -53,15 +53,15 @@ function addQuestion(user, img, search_name, text){
 
     var link = document.createElement("a");
     link.href = "/kweeni/"+search_name;
-    link.classList.add(".topics__question--p");
+    link.classList.add("topics__question--p");
     link.innerHTML = text;
     
     var author = document.createElement("p");
-    author.classList.add(".topics__question--author");
+    author.classList.add("topics__question--author");
     author.innerHTML = user;
 
     var userImage = document.createElement("img");
-    userImage.classList.add(".topics__question--img");
+    userImage.classList.add("topics__question--img");
     userImage.src = img;
 
     container.appendChild(blockLikes);
